377f243a914421dcb731b70d8e575bf3520edcbc
hojoon <hojoon.seok@lstgrp.com>
2021-01-19 18:02:11 +0900

    rename

--- db/migrate/002_create_app_keys.rb
+++ db/migrate/002_create_app_keys.rb
@@ -1,7 +1,7 @@
 class CreateAppKeys < ActiveRecord::Migration[4.2]
     def change
       create_table :app_keys do |k|
-        k.string : app_key
+        k.string :app_key
       end
     end
   end
