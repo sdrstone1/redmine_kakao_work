0809ed6382e3efac4f58bf0a5d8831352fc85827
hojoon <hojoon.seok@lstgrp.com>
2021-01-19 17:48:05 +0900

    rename

--- init.rb
+++ init.rb
@@ -1,11 +1,13 @@
-require 'redmine_webhook'
+require 'redmine_kakao_work'
 
-Redmine::Plugin.register :redmine_webhook do
-  name 'Redmine Webhook plugin'
-  author 'suer'
-  description 'A Redmine plugin posts webhook on creating and updating tickets'
+Redmine::Plugin.register :redmine_kakao_work do
+  name 'Redmine Kakao Work plugin'
+  author 'KollHong'
+  description 'A Redmine plugin webhooks on creating and updating articles based on https://github.com/suer/redmine_webhook'
   version '0.0.5'
-  url 'https://github.com/suer/redmine_webhook'
-  author_url 'http://d.hatena.ne.jp/suer'
+  url 'https://github.com/sdrstone1/redmine_kakao_work'
+  author_url 'https://kollhong.com/'
+  settings partial: 'settings/kakao_work/general'
+
   permission :manage_hook, {:webhook_settings => [:index, :show, :update, :create, :destroy]}, :require => :member
 end
