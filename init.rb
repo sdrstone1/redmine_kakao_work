2deb00c1a6f28649b5b76ad8464f0a353f3ccbdd
hojoon <hojoon.seok@lstgrp.com>
2021-01-19 18:59:58 +0900

    changed name of the plugin from redmine_webhook to redmine_kakao_work. add global admin settings page to set an App Key.

--- init.rb
+++ init.rb
@@ -1,11 +1,14 @@
-require 'redmine_webhook'
+require 'redmine_kakao_work'
 
-Redmine::Plugin.register :redmine_webhook do
-  name 'Redmine Webhook plugin'
-  author 'suer'
-  description 'A Redmine plugin posts webhook on creating and updating tickets'
+Redmine::Plugin.register :redmine_kakao_work do
+  name 'Redmine Kakao Work plugin'
+  author 'KollHong'
+  description 'A Redmine plugin webhooks on creating and updating articles based on https://github.com/suer/redmine_webhook'
   version '0.0.5'
-  url 'https://github.com/suer/redmine_webhook'
-  author_url 'http://d.hatena.ne.jp/suer'
+  url 'https://github.com/sdrstone1/redmine_kakao_work'
+  author_url 'https://kollhong.com/'
+  settings :default => { 'app_key' => "" },
+    partial: 'settings/kakao_work/general'
+
   permission :manage_hook, {:webhook_settings => [:index, :show, :update, :create, :destroy]}, :require => :member
 end
---
+++ lib/redmine_kakao_work.rb
@@ -0,0 +1,6 @@
+require 'redmine_kakao_work/projects_helper_patch'
+require 'redmine_kakao_work/issue_wrapper'
+require 'redmine_kakao_work/webhook_listener'
+
+module RedmineWebhook
+end
