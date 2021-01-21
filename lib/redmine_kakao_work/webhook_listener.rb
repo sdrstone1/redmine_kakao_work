83e0ae3c8d9cccc659bd83e708b4454b26b46367
hojoon <hojoon.seok@lstgrp.com>
2021-01-21 11:04:07 +0900

    updating wiki page now create a webhook request.

--- lib/redmine_kakao_work/webhook_listener.rb
+++ lib/redmine_kakao_work/webhook_listener.rb
@@ -42,6 +42,18 @@ module RedmineWebhook
       return unless webhooks
       post(webhooks, journal_to_json(issue, journal, controller))
     end
+    
+    def controller_wiki_edit_after_save(context = {})
+      return if skip_webhooks(context)
+      page = context[:page]
+      controller = context[:controller]
+      project = context[:project]
+      params = context[:params]
+      webhooks = Webhook.where(:project_id => project.id)
+      webhooks = Webhook.where(:project_id => 0) unless webhooks && webhooks.length > 0
+      return unless webhooks
+      post(webhooks, wiki_to_json(page, controller, project, params))
+    end
 
     def model_changeset_scan_commit_for_issue_ids_pre_issue_update(context = {})
       issue = context[:issue]
@@ -72,6 +84,74 @@ module RedmineWebhook
           :url => controller.nil? ? 'not yet implemented' : controller.issue_url(issue)
         }
       }.to_json
+    end
+
+    def wiki_to_json(page, controller, project, params)
+      content = params[:content]
+      text = content[:text]
+      comments = content[:comments]
+      {
+        :text => "Wiki #{controller.action_name} : #{project}",
+        :datas => [
+          { 
+            :type => "section",
+            :data => {
+              :content => "### 위키 페이지 #{controller.action_name}\r\n\r\n" +
+              "#### 프로젝트 #{project}의 #{page.title}\r\n" +
+              "**한줄설명** : #{comments}\r\n" +
+              "#{text}",
+              :type => "markdown"
+            }
+          }
+        ]
+      }.to_json
+        # page.attributes = {
+        #   "wiki_id"=>1,
+        #   "protected"=>false,
+        #   "parent_id"=>2,
+        #   "id"=>3,
+        #   "title"=>"Third",
+        #   "created_on"=>Wed, 20 Jan 2021 05:40:08 UTC +00:00
+        # }
+        # page.new_record = bool 
+              
+        # controller.action_name = update
+        # controller.params = {
+        #   "utf8"=>"✓",
+        #   "_method"=>"put",
+        #   "authenticity_token"=>"#",
+        #   "content"=><ActionController::Parameters {
+        #     "version"=>"1",
+        #     "text"=>"# Third\r\n\r\nwiki",
+        #     "comments"=>""
+        #   } permitted: false>,
+        #   "wiki_page"=><ActionController::Parameters {
+        #     "parent_id"=>"2"
+        #   } permitted: false>,
+        #   "commit"=>"Save",
+        #   "controller"=>"wiki",
+        #   "action"=>"update",
+        #   "project_id"=>"test-project",
+        #   "id"=>"Third"
+        # }
+        # project = title
+        # project.attributes = {
+        #   "id"=>1,
+        #   "name"=>"test project",
+        #   "description"=>"",
+        #   "homepage"=>"",
+        #   "is_public"=>true,
+        #   "parent_id"=>nil,
+        #   "created_on"=>Thu, 14 Jan 2021 07:25:46 UTC +00:00,
+        #   "updated_on"=>Thu, 14 Jan 2021 07:25:46 UTC +00:00,
+        #   "identifier"=>"test-project",
+        #   "status"=>1,
+        #   "lft"=>1,
+        #   "rgt"=>2,
+        #   "inherit_members"=>false,
+        #   "default_version_id"=>nil,
+        #   "default_assigned_to_id"=>nil
+        # }
     end
 
     def post(webhooks, request_body)
